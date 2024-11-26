ef1fddaf56b679e515a38eb328ed73afac10686e
kelvinnmuia <kelvinnmuia@gmail.com>
2024-11-26 21:58:35 +0300

    configured routing for the application

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router=createBrowserRouter([
+  {
+    path:'/auth/sign-in'
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
