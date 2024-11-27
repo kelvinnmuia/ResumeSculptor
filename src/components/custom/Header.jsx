9c44de963749f854191081ee7d3ff4e419558972
kelvinnmuia <kelvinnmuia@gmail.com>
2024-11-27 14:55:50 +0300

    added a button on the header section

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className='p-3 px-5'>
+      <img src='logo.svg' width={40} height={40} />
     </div>
   )
 }
